1ab2698fd20596d4e27ced294f4162a8900ddda3
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-06-28 11:53:55 -0600

    did some code to console log variable

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 30 + 20 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
