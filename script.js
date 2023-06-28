88dd5d9f69a62d93b7e3a413a988b1d8371d3262
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-06-28 17:06:16 -0600

    CHALLENGE 2 solution done

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ multiple
 lines`);
 
 /* ============= TAKING DECISIONS =============== */
-
+/* 
 const age = 15;
 
 if (age >= 18) {
@@ -184,3 +184,25 @@ if (birthYear <= 2000) {
 console.log(century);
 
 /* ============= CHALLENGE NUMBER 2 =============== */
+//Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+
+//"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+
+//2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+
+//Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+	console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+	console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
