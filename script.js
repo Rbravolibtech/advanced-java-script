82ed5db7a8e7db1a16a8c369de01e918a915074f
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-06-28 12:44:14 -0600

    writing clean code to none good code

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ console.log("Jonas");
 console.log(23);
 
 let firstName = "Jonas";
+
 console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+
+let job1 = "programmer";
+let job2 = "teacher";
