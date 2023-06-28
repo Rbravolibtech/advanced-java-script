9b64c0b8986b902a622cf59cbae54d7ceaa8e5f1
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-06-28 13:31:30 -0600

    made some empty variables

--- script.js
+++ script.js
@@ -27,9 +27,16 @@ console.log(myFirstJob);
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
-console.log(typeof true);
+//console.log(typeof true);
 console.log(typeof "javascriptIsFun");
-console.log(typeof 23);
-console.log(typeof "Jonas");
+//console.log(typeof 23);
+//console.log(typeof "Jonas");
 
 javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+/* UNDEFINED VARIABLE THAT DISPLAYS ON CONSOLE */
+
+let year;
+console.log(year);
+console.log(typeof year);
