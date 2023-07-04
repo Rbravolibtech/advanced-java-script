d64925c0debe8b18449bb1d1a4b73275f6150751
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-03 21:55:14 -0600

    conditional operators

--- script.js
+++ script.js
@@ -365,7 +365,7 @@ if (day === "monday") {
 */
 
 /* =============== STATEMENTS AND EXPRESSIONS =============== */
-
+/* 
 3 + 4;
 1991;
 true && false && !false;
@@ -375,3 +375,25 @@ if (23 > 10) {
 }
 const me = "Jonas";
 console.log(`I'm ${2037 - 1991} years old ${me}`);
+ */
+/* =============== CONDITIONAL TERNARY OPERATOR =============== */
+
+const age = 23;
+//age >= 18
+//? console.log("I like to drink wine")
+//: console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+	drink2 = "wine";
+} else {
+	drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+
+/* =============== CHALLENGE 4 =============== */
